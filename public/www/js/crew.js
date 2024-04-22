123cfd3e2982410dda7e60ffa86fbb6973e2e9d6
lha901222 <lha901222@gmail.com>
2024-04-22 17:56:48 +0900

    feature : 목록 json 연동 처리

--- public/www/js/crew.js
+++ public/www/js/crew.js
@@ -1,40 +1,76 @@
 window.addEventListener("load", function () {
-  const dataUrl = "./apis/crew.json";
-  fetch(dataUrl)
-    .then((response) => {
-      console.log(response);
-      const result = response.json();
-      return result;
-    })
-    .then((result) => {
-      let allTag = "";
-      const crew = document.querySelector("#crew");
-      for (let i = 0; i < result.length; i++) {
-        const obj = result[i];
-        const tag = `<a href="${obj.link}" class="list-box">
+  // 함수로 만들어보자 (코드가 너무 길고 복잡해서)
+  // 호이스팅을 방지하고자 위쪽에서 만들자.
+
+  // html에 내용 출력하기
+  const makeHtml = (_result) => {
+    let allTag = "";
+    const crew = document.querySelector("#crew");
+    // 최적화를 위해 한번만 찾으러~
+    const total = _result.length;
+
+    // for (let i = 0; i < total; i++) {
+    //   const obj = _result[i];
+    // const tag = `<a href="${obj.link}" class="list-box">
+    //   <div
+    //     class="list-box-img br-20"
+    //     style="
+    //       background: url('./images/${obj.imgpath}') no-repeat center;
+    //       background-size: cover;
+    //     "
+    //   ></div>
+    //   <div class="list-box-cate">
+    //     <img
+    //       src="./images/icon/${obj.icon}"
+    //       alt="${obj.category}"
+    //     />
+    //     <span style="color:${obj.txtcolor};">${obj.category}</span>
+    //   </div>
+    //   <p class="list-box-title">
+    //   ${obj.title}
+    //   </p>
+    //   <span class="list-box-day">${obj.day}</span>
+    // </a>`;
+    // allTag += tag;
+    // }
+
+    // 추천방법(조건은 대상이 배열인 경우)
+    // _result.foreach((요소,번호,원본)=>{})
+    _result.forEach((item) => {
+      const tag = `<a href="${item.link}" class="list-box">
         <div
           class="list-box-img br-20"
           style="
-            background: url('./images/${obj.imgpath}') no-repeat center;
+            background: url('./images/${item.imgpath}') no-repeat center;
             background-size: cover;
           "
         ></div>
         <div class="list-box-cate">
           <img
-            src="./images/icon/${obj.icon}"
-            alt="${obj.category}"
+            src="./images/icon/${item.icon}"
+            alt="${item.category}"
           />
-          <span style="color:${obj.txtcolor};">${obj.category}</span>
+          <span style="color:${item.txtcolor};">${item.category}</span>
         </div>
         <p class="list-box-title">
-        ${obj.title}
+        ${item.title}
         </p>
-        <span class="list-box-day">${obj.day}</span>
+        <span class="list-box-day">${item.day}</span>
       </a>`;
-        allTag += tag;
-        // console.log(allTag);
-      }
-      crew.innerHTML = allTag;
+      allTag += tag;
+    });
+
+    crew.innerHTML = allTag;
+  };
+
+  const dataUrl = "./apis/crew.json";
+  fetch(dataUrl)
+    .then((response) => {
+      const result = response.json();
+      return result;
+    })
+    .then((result) => {
+      makeHtml(result);
     })
     .catch((error) => {});
 });
