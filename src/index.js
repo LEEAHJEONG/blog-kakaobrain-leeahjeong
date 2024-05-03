4eaf34b3987fea767d729f28ebe444a1117ef7b1
lha901222 <lha901222>
2024-05-03 16:51:35 +0900

    feature : 마이그레이션 진행 중

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import MbHeader from "./components/MbHeader";
+import PopUp from "./components/PopUp";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import "./css/reset.css";
+import "./css/common.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <PopUp></PopUp>
+    <div className="wrap">
+      <Header></Header>
+      <MbHeader></MbHeader>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
